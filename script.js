c07ce4b0f698072f0dae2d74a8f713af5e272010
John <103902630+JohnCJr@users.noreply.github.com>
2023-07-30 13:52:01 -0400

    Created new variable for the creation of new divs
    The row variable, given a class name of row, is created in the outer
    loop. The variable's inner html will contain the result of a loop
    iteration. The row will then be appended to the container div.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 "use strict";
 
 const container = document.querySelector(".container");
-const gridDimension = 100;
+const gridDimension = 50;
 let tile = "";
 
-// loops to create 16x16 tiles inside the container div
+// loops to create row divs that will contain the tiles
 for (let i = 0; i < gridDimension; i++) {
-  tile += `<div class = "tile num${i + 1}"></div>`;
-  container.innerHTML = tile;
+  const row = document.createElement("div"); // creates new div
+  row.setAttribute("class", "row"); // gives the div a class name of row
+
+  tile = ""; // resets the tile content
+
+  // loop will fill tile content with the specified number of divs
+  for (let j = 0; j < gridDimension; j++) {
+    tile += `<div class = "tile num${j + 1}"></div>`;
+  }
+  row.innerHTML = tile;
+
+  container.appendChild(row);
 }
