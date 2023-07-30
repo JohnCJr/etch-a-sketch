3a3466c67af28cf79ca148e447b1c84a33fc60dd
John <103902630+JohnCJr@users.noreply.github.com>
2023-07-30 17:33:27 -0400

    Created function to get user input
    The function, named setUserDimension, gets the users input and returns
    it. This number will be used to set the gridDimension. The function will
    loop until a valid input is entered.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 "use strict";
 
 const container = document.querySelector(".container");
-const gridDimension = 50;
+let gridDimension = 50;
 let tile = "";
+
+function setUserDimension() {
+  let gridCount = Number(
+    prompt(
+      "Enter the number squares per side of the grid. Number cannot be less than 1 or greater than 100."
+    )
+  );
+
+  while (gridCount < 1 || gridCount > 100 || !Number.isInteger(gridCount)) {
+    alert(
+      "Invalid input. Value must be a whole number between 1 and 100. Try again"
+    );
+    gridCount = Number(
+      prompt(
+        "Enter the number squares per side of the grid. Number cannot be less than 1 or greater than 100."
+      )
+    );
+  }
+  return gridCount;
+}
 
 // loops to create row divs that will contain the tiles
 for (let i = 0; i < gridDimension; i++) {
