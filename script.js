7e47426f76ef19a0b4df5679c925167e7b3a0f93
John <103902630+JohnCJr@users.noreply.github.com>
2023-08-01 21:41:16 -0400

    Fixed setUserDimension function and event listener
    The setUserDimension function caused an issue where clicking the cancel
    button on the prompt wouldn't close the prompt. An if statement was
    added to check if the result is null, which is the value returned when
    the cancel button is clicked. This fix caused an issue with the event
    listener for the button that sets a new grid based on the previously
    mentioned function. Clicking cancel would delete the entire grid. This
    issue was solved by moving the gridDimension to the top of the listener
    and adding a if statement that prevents the other functions from running
    if the value is null.

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ let gridDimension = 50;
 let tile = "";
 
 function setUserDimension() {
-  let gridCount = Number(
-    prompt(
-      "Enter the number squares per side of the grid. Number cannot be less than 1 or greater than 100."
-    )
+  let gridCount = prompt(
+    "Enter the number squares per side of the grid. Number cannot be less than 1 or greater than 100."
   );
 
-  while (gridCount < 1 || gridCount > 100 || !Number.isInteger(gridCount)) {
-    alert(
-      "Invalid input. Value must be a whole number between 1 and 100. Try again"
-    );
-    gridCount = Number(
-      prompt(
-        "Enter the number squares per side of the grid. Number cannot be less than 1 or greater than 100."
-      )
-    );
+  if (gridCount !== null) {
+    gridCount = Number(gridCount);
+    while (gridCount < 1 || gridCount > 100 || !Number.isInteger(gridCount)) {
+      alert(
+        "Invalid input. Value must be a whole number between 1 and 100. Try again"
+      );
+      gridCount = Number(
+        prompt(
+          "Enter the number squares per side of the grid. Number cannot be less than 1 or greater than 100."
+        )
+      );
+    }
   }
   return gridCount;
 }
@@ -44,7 +45,6 @@ function setGrid(currentGrid) {
 
 function clearGrid() {
   let previousRows = document.querySelectorAll(".row");
-  console.log(previousRows);
   previousRows.forEach((row) => {
     row.remove();
   });
@@ -103,12 +103,16 @@ const setGridButton = document.querySelector("button");
 setTileEvent(allTiles);
 
 setGridButton.addEventListener("click", () => {
-  clearGrid();
-
   gridDimension = setUserDimension();
-  setGrid(gridDimension);
 
-  allTiles = document.querySelectorAll(".tile");
+  // won't trigger functions to clear adn create new grid
+  if (gridDimension !== null) {
+    clearGrid();
 
-  setTileEvent(allTiles);
+    setGrid(gridDimension);
+
+    allTiles = document.querySelectorAll(".tile");
+
+    setTileEvent(allTiles);
+  }
 });
